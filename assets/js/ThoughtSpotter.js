76d3d16c327a2d9326e286151f6cb984f727c055
Andrew Carreiro <andrew@headfirst.ca>
2014-11-09 01:07:59 -0500

    more panel work

--- assets/js/ThoughtSpotter.js
+++ assets/js/ThoughtSpotter.js
@@ -6,6 +6,16 @@ ThoughtSpotter = {
 		}else if ( typeof(arg) == 'number' ) {
 			for (var i=0; i<thoughtSpots.length; i++) {
 				if ( thoughtSpots[i]["#"] == arg ) {
+						var classes = thoughtSpots[i]["CATEGORY"];
+						classes = classes.replace("Health and Social Services","health");
+						classes = classes.replace("Recreation and Culture","culture");
+						classes = classes.replace("Spirituality and Wellbeing","spirituality");
+						classes = classes.replace("Family and Friends","family");
+						classes = classes.replace("Work and School","work");
+						classes = classes.replace("Sex and Relationships","relationships");
+						classes = classes.replace("Legal and Financial","legal");
+						classes = classes.split(", ");
+						thoughtSpots[i].classes = classes;
 					return thoughtSpots[i];
 				}
 			}
